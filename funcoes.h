9fc20748d6874b0382b18ea8b7c68ec6628ad3e5
Júlio César <52138888+julnox@users.noreply.github.com>
2022-07-10 09:56:38 -0300

    Separated myjonaslist.h into two header files

---
+++ funcoes.h
@@ -0,0 +1,11 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+
+void checarLocal();
+void listarTodasAsObras ();
+void listarObras (unsigned int acaoListar);
+void adicionarObra (unsigned int acaoAdicionar);
+void removerObra(unsigned int acaoRemover);
+void pesquisarObra(unsigned int acaoPesquisar);
+
+#endif
---
+++ menus.h
@@ -0,0 +1,10 @@
+#ifndef MENUS_H
+#define MENUS_H
+
+unsigned int menuPrincipal ();
+unsigned int menuListar();
+unsigned int menuAdicionar ();
+unsigned int menuRemover();
+unsigned int menuPesquisar ();
+
+#endif
